84f6d600d11f857e80c3f53a6aa473845385cc9a
J <j@jabl3s.uk>
2023-09-01 02:20:25 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# jmux
+# jmux - ((tmux wrapper))  
+  
+Step1) {sudo apt-get install sshpass tmux ssh-askpass} ((Install dependencies))  
+Step2) Add {source /<path>/<to>/jmux.sh} to {~/.bashrc}  
+Step3) Reload bashrc with {source ~./bashrc}  
+Step4...)  
+4.1) jmuxconn <ssh_username> <ssh_password> <ipadresses:LIST_THEM_WITH_SPACES>    
+4.2) jmuxcomm <number_of_succesful_opened_tmux_panes_depending_on_vertical_screen_space>     
+4.3) jmuxdiss ((Kills-the-jmuxconn-and-tmux-session)) 
+  
+4.1.e.g.) jmuxconn ubuntu rootpassword 192.168.1.1 192.168.1.2 192.168.1.3 192.168.1.4 
+4.2.e.g.) jmuxcomm 4 echo "wassup from tmux"  
+![Alt text](/assets/images/image-1.png) 
+
+
+
+
---
+++ jmux.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#ADD TO BASHRC:
+#source /home/ubuntu/jmux.sh
+# Define a "class" to manage tmux sessions with SSH connections
+function jmuxdiss() {
+    tmux kill-session -t jsession
+}
+
+function jmuxconn() {
+    local username="$1"
+    local password="$1"
+    shift 2
+    tmux new-session -d -s jsession
+    for ip in "$@"; do
+        tmux split-window -v "sshpass -p $password ssh $username@$ip"
+        tmux select-layout even-vertical
+    done
+    tmux attach-session -t jsession:0.0
+    }
+
+function jmuxcomm() {
+    local servercount="$1"
+    shift
+    local cmd="" 
+    for word in "$@"; do
+        cmd="$cmd $word"
+    done
+    for ((i=1; ; i++)); do
+    # You can add your commands here
+    tmux send-keys -t "jsession:0.$i" "$cmd" C-m
+    # Add a break condition if needed
+    # For example, to stop after 10 iterations
+    if [ $i -eq $servercount ]; then
+        break
+    fi
+    done
+    }
