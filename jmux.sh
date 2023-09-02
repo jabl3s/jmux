b23614c3d2d379ece668d2e240c11d7fa6fedf5d
J <j@jabl3s.uk>
2023-09-03 00:34:02 +0100

    -o StrictHostKeyChecking=no

--- jmux.sh
+++ jmux.sh
@@ -74,12 +74,12 @@ function jmux_connect() { #USE LIKE: jmuxconnect user@ip..user@ip
     else
         read -s -p "Enter the password being used on all these servers:" serverpass  
         # Create a new tmux session named "jsession"
-        tmux new-session -d -s jsession "sshpass -p $serverpass ssh $1"
+        tmux new-session -d -s jsession "sshpass -p $serverpass ssh -o StrictHostKeyChecking=no $1"
         # Shift the arguments to remove the first IP address
         shift
         # Loop through the remaining IP addresses and create vertical splits
         for ip in "$@"; do
-            tmux split-window -v "sshpass -p $serverpass ssh $ip"
+            tmux split-window -v "sshpass -p $serverpass ssh -o StrictHostKeyChecking=no $ip"
         done
         # Set the layout to even-vertical
         tmux select-layout even-vertical
