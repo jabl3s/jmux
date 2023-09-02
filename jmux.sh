3a0a368fe55d81efaf9d9d24891657447be1df7b
J <j@jabl3s.uk>
2023-09-02 08:24:43 +0100

    update via curl no server cache pls

--- jmux.sh
+++ jmux.sh
@@ -102,7 +102,7 @@ function jmux_update(){
         echo "$line_to_check" >> ~/.bashrc
         echo "Added the line to ~/.bashrc."
     fi
-    curl -o ~/jmux.sh https://raw.githubusercontent.com/jabl3s/jmux/main/jmux.sh && source ~/.bashrc
+    curl -H "Cache-Control: no-cache" -o ~/jmux.sh https://raw.githubusercontent.com/jabl3s/jmux/main/jmux.sh && source ~/.bashrc
 }
 function jmux_rke(){
         # Specify the path to the YAML file
