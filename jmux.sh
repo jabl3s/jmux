69ce10ba5c8f66b5bc0311b27fb592551429c165
J <j@jabl3s.uk>
2023-09-02 12:50:23 +0100

    sense

--- jmux.sh
+++ jmux.sh
@@ -3,9 +3,9 @@
 declare -A command_descriptions
 # Define the key-value pairs (commands as keys and descriptions as values)
 command_descriptions["connect"]="Replace 'ssh user@ip' with 'jmux connect user@ip user@ip user@ip user@ip' (limited to four ssh sessions.)"
+command_descriptions["disconnect"]="All ssh connections end and jmux session ends, returning shell to normal. Now provide all user@ip to jmux connect to start again."
 command_descriptions["command"]="[number_of_ssh] ...leave blank to send ctrl+c, ...pipe password in for sudo commands, see example of this in read me."
-command_descriptions["hide"]="Runs all active ssh sessions in background, reconnect with jmux connect"
-command_descriptions["disconnect"]="All ssh connections end and jmux session ends, termination)"
+command_descriptions["hide"]="Runs all active ssh sessions in background, reconnect with just 'jmux connect' do not provide user@ip unless jmux disconnect was run, otherwise buggy gui."
 command_descriptions["dependencies"]="Run this to get everything needed for jmux to work)"
 command_descriptions["update"]="Gets the latest jmux version from jabl3s git"
 command_descriptions["more"]="Extend this prompt with more commands and additional information like current the limitations of jmux"
